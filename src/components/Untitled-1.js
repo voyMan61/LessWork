aa6e622c32f3c0503a993b2a3f1a9a8632858e59
voyMan61 <36798596+voyMan61@users.noreply.github.com>
2018-07-19 13:34:23 +0800

    Revert "mewTwo did nothing wrong"
    This reverts commit 0cf73e14dbb4add2d429e50f0ad8aba9029d1509.

--- src/components/Untitled-1.js
+++ src/components/Untitled-1.js
@@ -1,122 +1,54 @@
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 88,
-      "density": {
-        "enable": true,
-        "value_area": 700
-      }
-    },
-    "color": {
-      "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 15
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1.5,
-        "opacity_min": 0.15,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 2.5,
-      "random": false,
-      "anim": {
-        "enable": true,
-        "speed": 2,
-        "size_min": 0.15,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 110,
-      "color": "#33b1f8",
-      "opacity": 0.25,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 1.6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
+class Counter extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      currentCount: 0
     }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
   }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);;
+  
+  // Basic parent increment handler
+  increment() {
+    this.setState({
+      currentCount: this.state.currentCount+1
+    })
+  }
+  
+  // Basic parent decrement handler
+  decrement() {
+    this.setState({
+      currentCount: this.state.currentCount-1
+    })
+  }
+  
+  render() {
+    // Here we are passing references to the increment and decrement functions to our child components as props. 
+    return(
+      <div>
+        <h2>Current Count: {this.state.currentCount}</h2>
+        <Increment increment={this.increment.bind(this)} />
+        <Decrement decrement={this.decrement.bind(this)} />
+      </div>
+    );
+  }
+}
+
+class Increment extends React.Component {
+  // This allows us to change the state of the parent component when we click the button in the child component using this.props.increment
+  render() {
+    return(
+      <button onClick={this.props.increment}>Higher</button>
+    );
+  }
+}
+
+class Decrement extends React.Component {
+  // And again for this.props.decrement
+  render() {
+    return(
+      <button onClick={this.props.decrement}>Lower</button>
+    );
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
